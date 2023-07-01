797f18c311b14e9056c390a13a8577bf93e18bb3
Guido Alejandro Saavedra Torres <galejandrosaavedra@gmail.com>
2023-07-01 13:11:31 -0400

    Requisitos E4 Completados

---
+++ RequisitosE4/djaTOdoproject/principal/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from .views import delete_task
+
+urlpatterns = [
+    path('', views.TaskListView.as_view(), name='home'),
+    path('crear_tarea/', views.CreateTask.as_view(), name='crear_tarea'),
+    path('tarea/<int:pk>/', views.DetailsTask.as_view(), name='tarea_detalles'),
+    path('tarea/<int:pk>/editar/', views.EditTask.as_view(), name='editar_tarea'),
+    path('finalizar/', views.FinishTask.as_view(), name='complete_task'),
+    path('tarea/<int:pk>/eliminar/', delete_task, name='eliminar_tarea'),
+    ]
+    
+    
